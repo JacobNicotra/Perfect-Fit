4e73ae6885d431613dfe84b9449347fb759bba6e
JacobNicotra <nicotra1008@gmail.com>
2022-01-25 11:59:36 -0600

    swap backend complete

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.puzzle_routes import puzzle_routes
+from .api.swap_routes import swap_routes
 
 from .seeds import seed_commands
 
@@ -33,6 +34,7 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(puzzle_routes, url_prefix='/api/puzzles')
+app.register_blueprint(swap_routes, url_prefix='/api/swaps')
 
 db.init_app(app)
 Migrate(app, db)
